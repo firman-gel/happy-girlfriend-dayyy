43004f1e225844305a9e62f84d5944d940a6c3f3
firman-gel <gellat0o_firman@apps.ipb.ac.id>
2025-08-01 00:19:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = (Math.random() * 2 + 3) + "s";
+    body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}
+
+setInterval(createHeart, 500);
